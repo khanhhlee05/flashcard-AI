a656621f5bfe77fe355d3cf82542aad8ad164d75
Khanh Le <kle01@villanova.edu>
2024-08-15 21:07:37 +0700

    Add some details on landing page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import getStripe from "./utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material"
+import { AppBar, Button, Container, Toolbar, Typography, Box, Grid } from "@mui/material"
 import Head from "next/head";
 
 
 
 export default function Home() {
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="100vw">
       <Head>
         <title>Flashcard SaaS</title>
         <meta name="description" content="Flashcard SaaS created flashcards from your text" />
@@ -16,13 +16,13 @@ export default function Home() {
 
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">
+          <Typography variant="h6" style={{flexGrow: 1}}>
             Flashcard SaaS
           </Typography>
 
           <SignedOut>
-            <Button>Login</Button>
-            <Button>Signup</Button>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Signup</Button>
           </SignedOut>
 
           <SignedIn>
@@ -31,6 +31,46 @@ export default function Home() {
 
         </Toolbar>
       </AppBar>
+
+      <Box sx={{textAlign:"center", my:4}}>
+        <Typography variant="h2">
+          Welcome to Flashcard SaaS!
+        </Typography>
+
+        <Typography variant="h5">
+          {" "}
+          The easiest to make flashcards from your text
+        </Typography>
+
+        <Button variant="contained" color="primary" sx={{mt:2}}>
+          Get Started
+        </Button>
+      </Box>
+
+      <Box sx={{my:6}}>
+        <Typography variant="h4" components="h2">
+          Features
+        </Typography>
+
+        <Grid contained spacing={4}>
+          <Grid item xs={12} md = {4}>
+            <Typography variant="h6">Easy Text Input</Typography>
+            {""}
+            <Typography>Simply input your text andx let us do the rest. Creating flashcards has neverbeen easiers</Typography>
+          </Grid>
+          <Grid item xs={12} md = {4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            {""}
+            <Typography>Our AI intelligently breaks down your text into concise flashcards, perfect for quality studying sessions</Typography>
+          </Grid>
+          <Grid item xs={12} md = {4}>
+            <Typography variant="h6">Accessible Anywhere</Typography>
+            {""}
+            <Typography>Access your flashcards from any device, at any time. Study on the go with ease</Typography>
+          </Grid>
+        </Grid>
+
+      </Box>
     </Container>
   );
 }
