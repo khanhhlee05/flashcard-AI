c8a17ec555b566ea7406f81d3a548f813826424d
Khanh Le <kle01@villanova.edu>
2024-08-17 23:52:57 +0700

    page js and save func by firebase

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,5 +1,8 @@
 "use client"
 
+import { db } from "@/firebase"
+import { writeBatch } from "firebase/firestore"
+
 export default function Generate(){
     const {isLoaded, isSignedIn, user} = useUser()
     const [flashcards, setFlashcards] = useState([])
@@ -31,6 +34,39 @@ export default function Generate(){
 
     const handleClose = () => {
         setOpen(false)
+    }
+
+    const saveFlashcards = async () => {
+        if (!name) {
+            alert("Please enter a name for your flashcards")
+            return
+        }
+        const batch = writeBatch(db)
+        const userDocRef = doc(collection(db, "users"), user.id)
+        const docSnap = await getDoc(userDocRef)
+
+        if (docSnap.exists()){
+            const collections = docSnap.data().flashcards || []
+            if (collections.find((f) => f.name === name )){
+                alert("Flashcards with this name already exist")
+                return 
+            } else {
+                collections.push({name})
+                batch.set(userDocRef, {flashcards: collections}, {merge : true})
+            }
+        } else {
+            batch.set(userDocRef, {flashcards: [{name}]})
+        }
+
+        const columnRef = collection(userDocRef, name)
+        flashcards.forEach((flashcard) => {
+            const cardDocRef = doc(colRef)
+            batch.set(cardDocRef, flashcard)
+        })
+
+        await batch.commit()
+        handleClose()
+        router.push("/flashcards")
     }
 
 
