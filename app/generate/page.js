2470d8b26535c5e57b3f662e11be84f9d28f1b7f
Khanh Le <kle01@villanova.edu>
2024-08-18 19:22:51 +0700

    fix bug in gen

--- app/generate/page.js
+++ app/generate/page.js
@@ -101,61 +101,62 @@ export default function Generate() {
                     {flashcards.map((flashcard, index) => (
                         <Grid item xs={12} sm={6} md={4} key={index}>
                             <Card>
-                                <CardActionArea onClick={() => { handleCardClick(index) }}></CardActionArea>
-                                <CardContent>
-                                    <Box
-                                    sx={{
-                                        perspective: '1000px',
-                  
-                                        '& > div':{
-                                        transition: 'transform 0.6s',
-                                        transformStyle: 'preserve-3d',
-                                        position: 'relative',
-                                        width: '100%',
-                                        height: '200px',
-                                        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-                                        transform: flipped[index]
-                                        ? 'rotateY(180deg)' 
-                                        : 'rotateY(0deg)',
-                                        },
-                  
-                                        '& > div > div':{
-                                        position: 'absolute',
-                                        width: '100%',
-                                        height: '100%',
-                                        backfaceVisibility: 'hidden',
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        padding: 2,
-                                        boxSizing: 'border-box'
-                                        },
-                  
-                                        '& > div div:nth-of-type(2)':{
-                                        transform: 'rotateY(180deg)'
-                                        }
-                                      
-                                    }}>
-                                        <div>
+                                <CardActionArea onClick={() => { handleCardClick(index) }}>
+                                    <CardContent>
+                                        <Box
+                                            sx={{
+                                                perspective: '1000px',
+
+                                                '& > div': {
+                                                    transition: 'transform 0.6s',
+                                                    transformStyle: 'preserve-3d',
+                                                    position: 'relative',
+                                                    width: '100%',
+                                                    height: '200px',
+                                                    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+                                                    transform: flipped[index]
+                                                        ? 'rotateY(180deg)'
+                                                        : 'rotateY(0deg)',
+                                                },
+
+                                                '& > div > div': {
+                                                    position: 'absolute',
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    backfaceVisibility: 'hidden',
+                                                    display: 'flex',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                    padding: 2,
+                                                    boxSizing: 'border-box'
+                                                },
+
+                                                '& > div div:nth-of-type(2)': {
+                                                    transform: 'rotateY(180deg)'
+                                                }
+
+                                            }}>
                                             <div>
-                                                <Typography variant="h5" component="dev">
-                                                    {flashcard.front}
-                                                </Typography>
+                                                <div>
+                                                    <Typography variant="h5" component="dev">
+                                                        {flashcard.front}
+                                                    </Typography>
+                                                </div>
+                                                <div>
+                                                    <Typography variant="h5" component="dev">
+                                                        {flashcard.back}
+                                                    </Typography>
+                                                </div>
                                             </div>
-                                            <div>
-                                                <Typography variant="h5" component="dev">
-                                                    {flashcard.back}
-                                                </Typography>
-                                            </div>
-                                        </div>
-                                        
-                                    </Box>
-                                </CardContent>
+
+                                        </Box>
+                                    </CardContent>
+                                </CardActionArea>
                             </Card>
                         </Grid>
                     ))}
                 </Grid>
-                <Box sx ={{mt:4, display: "flex", justifyContent: "center"}}>
+                <Box sx={{ mt: 4, display: "flex", justifyContent: "center" }}>
                     <Button variant="contained" color="secondary" onClick={handleOpen}>
                         Save
                     </Button>
@@ -163,7 +164,7 @@ export default function Generate() {
             </Box>
         )}
 
-        <Dialog open = {open} onClose = {handleClose}>
+        <Dialog open={open} onClose={handleClose}>
             <DialogTitle>Save Flashcards</DialogTitle>
             <DialogContent>
                 <DialogContentText>
